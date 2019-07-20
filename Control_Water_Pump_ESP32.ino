f5f7bb3a2167ce6fb8c26f7dca176a254533d430
taninwat <53119440+taninwat@users.noreply.github.com>
2019-07-21 01:15:28 +0700

    Update and rename Control_Water_Pump_ESP32.ino.ino to Control_Water_Pump_ESP32.ino

---
+++ Control_Water_Pump_ESP32.ino
@@ -0,0 +1,50 @@
+#include <WiFi.h> //https://www.arduino.cc/en/Reference/WiFi
+
+#define WIFI_NAME "TVS_3BB 2.4G"
+#define WIFI_PASS "0955167824"
+
+#define WATER_PUMP_PIN 23
+
+WiFiServer server(11008);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.mode(WIFI_STA);
+    Serial.println("Attempting to connect to SSID: ");
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(WIFI_NAME, WIFI_PASS);
+      for (int i = 0; i < 10; i++) {
+        Serial.print(".");
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        delay(500);
+      }
+    }
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
+  }
+
+  WiFiClient client = server.available();
+
+  while (client.connected()) {
+    Serial.println("Client Connected");
+    Serial.println("Turn On");
+    digitalWrite(WATER_PUMP_PIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(5000);
+  }
+  digitalWrite(WATER_PUMP_PIN, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("TURN OFF");
+  delay(5000);
+
+}
